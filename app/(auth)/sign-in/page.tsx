0bab4d8503780e124c2b39880de4ed5a49b5a3d3
Helblze <helblzekiev@gmail.com>
2024-08-17 20:25:25 +0530

    Authentication Form creation completed

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const page = () => {
+const signIn = () => {
   return (
-    <div>
-      signin
-    </div>
+    <section className='flex-center size-full max-sm:px-6'>
+      <AuthForm type="sign-in"/>
+    </section>
   )
 }
 
-export default page
+export default signIn
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,13 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const page = () => {
+const signUp = () => {
   return (
-    <div>
-      signup
-    </div>
+    <section className='flex-center size-full max-sm:px-6'>
+      <AuthForm type="sign-up"/>
+    </section>
   )
 }
 
-export default page
+export default signUp
+
